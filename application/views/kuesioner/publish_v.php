8e0d6b348ce5778a8cefaf1caa098b1346383de8
ujshandi@gmail.com <ujshandi@gmail.com@dfb6e4cd-bd51-6a30-0288-f90842ca8643>
2015-01-08 11:16:29 +0000

    progress publish kuesioner

--- application/views/kuesioner/publish_v.php
+++ application/views/kuesioner/publish_v.php
@@ -3,14 +3,20 @@
 		
 	    <link href="<?=base_url('static')?>/js/iCheck/skins/square/square.css" rel="stylesheet">
 		<style>
+			.wizard, .tabcontrol {
+				 
+				width: 95%;
+			}
+			.wizard > .steps > ul > li {
+				width: 20%;
+			}
 			.wizard > .content > .body{
 				position:unset;
 			}
 		</style>
         <!-- page start-->
                 
-        <div class="row">
-            <div class="col-sm-12">
+        <div class="panel-body"> 
                 <section class="panel">
                     <header class="panel-heading">
                         <b>PREVIEW DAFTAR PERTANYAAN KUESIONER</b><br>
@@ -27,8 +33,7 @@
                             <?=$data?>
 						</div> 
                     </div>
-                </section>
-            </div>
+                </section> 
         </div>
     <!--main content end-->
 	 
@@ -45,7 +50,7 @@
 					bodyTag: "section",
 					transitionEffect: "slideLeft",
 					onInit : function (event, currentIndex) {
-						alert('kadie');
+						//alert('kadie');
 						$('.square input').iCheck({
 							checkboxClass: 'icheckbox_square',
 							radioClass: 'iradio_square',

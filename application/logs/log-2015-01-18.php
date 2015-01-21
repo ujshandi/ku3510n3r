896f60cbdc12ee1cc66c71abdef645fbd05ed0bf
ariefekaputra09@gmail.com <ariefekaputra09@gmail.com@dfb6e4cd-bd51-6a30-0288-f90842ca8643>
2015-01-21 16:35:42 +0000

    Tambahan untuk perbaikan master alumni, tambah hapus edit, untuk yg import dan menu diklat sedang dalam proses pengerjaan, akan segera diselesaikan

---
+++ application/logs/log-2015-01-18.php
@@ -0,0 +1,20 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-01-18 20:50:31 --> Severity: Warning  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: Access denied for user 'root'@'localhost' (using password: YES) E:\Server\xampp\htdocs\e-kuesioner\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-18 20:50:31 --> Unable to connect to the database
+ERROR - 2015-01-18 20:57:13 --> Query error: Table 'minerba_kuesioner.menu' doesn't exist
+ERROR - 2015-01-18 21:20:34 --> 404 Page Not Found --> diklat
+ERROR - 2015-01-18 21:22:48 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined index: page E:\Server\xampp\htdocs\e-kuesioner\application\libraries\template.php 9
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: pg_aktif E:\Server\xampp\htdocs\e-kuesioner\application\views\template\inc_header.php 25
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: pg_aktif E:\Server\xampp\htdocs\e-kuesioner\application\views\template\inc_header.php 31
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined index: sd_left E:\Server\xampp\htdocs\e-kuesioner\application\libraries\template.php 11
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: cur_menu E:\Server\xampp\htdocs\e-kuesioner\application\views\template\sidebar_left.php 30
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: cur_menu E:\Server\xampp\htdocs\e-kuesioner\application\views\template\sidebar_left.php 30
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: cur_menu E:\Server\xampp\htdocs\e-kuesioner\application\views\template\sidebar_left.php 30
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: cur_menu E:\Server\xampp\htdocs\e-kuesioner\application\views\template\sidebar_left.php 30
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined index: page E:\Server\xampp\htdocs\e-kuesioner\application\libraries\template.php 13
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: pg_aktif E:\Server\xampp\htdocs\e-kuesioner\application\views\template\inc_footer.php 22
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: pg_aktif E:\Server\xampp\htdocs\e-kuesioner\application\views\template\inc_footer.php 47
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: pg_aktif E:\Server\xampp\htdocs\e-kuesioner\application\views\template\inc_footer.php 58
+ERROR - 2015-01-18 21:23:22 --> Severity: Notice  --> Undefined variable: pg_aktif E:\Server\xampp\htdocs\e-kuesioner\application\views\template\inc_footer.php 68
---
+++ application/logs/log-2015-01-21.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-01-21 21:49:28 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
+ERROR - 2015-01-21 21:49:50 --> Problem Inserting to :  (0)
+ERROR - 2015-01-21 21:50:31 --> Query error: Unknown column 'instansi_id' in 'field list'
+ERROR - 2015-01-21 21:55:02 --> Query error: Unknown column 'r.instansi_id' in 'where clause'
+ERROR - 2015-01-21 21:56:52 --> Query error: Unknown column 'r.instansi_id' in 'where clause'
+ERROR - 2015-01-21 21:58:08 --> Query error: Unknown column 'r.instansi_id' in 'where clause'
+ERROR - 2015-01-21 21:59:32 --> Query error: Unknown column 'r.instansi_id' in 'where clause'
+ERROR - 2015-01-21 22:00:14 --> Query error: Table 'minerba_kuesioner.pertanyaan_opsi' doesn't exist
+ERROR - 2015-01-21 22:00:23 --> Query error: Table 'minerba_kuesioner.pertanyaan_opsi' doesn't exist
+ERROR - 2015-01-21 22:06:29 --> Query error: Unknown column 'nik' in 'field list'
+ERROR - 2015-01-21 22:06:32 --> Query error: Unknown column 'nik' in 'field list'
+ERROR - 2015-01-21 22:07:48 --> Query error: Unknown column 'nik' in 'field list'
+ERROR - 2015-01-21 22:08:15 --> Query error: Unknown column 'nik' in 'field list'
+ERROR - 2015-01-21 22:13:09 --> Query error: Unknown column 'r.instansi_id' in 'on clause'
+ERROR - 2015-01-21 22:13:38 --> Query error: Unknown column 'r.instansi_id' in 'on clause'
+ERROR - 2015-01-21 22:13:50 --> Query error: Unknown column 'r.instansi_id' in 'on clause'
+ERROR - 2015-01-21 22:19:56 --> Query error: Unknown column 'r.instansi_id' in 'on clause'
+ERROR - 2015-01-21 22:45:50 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
+ERROR - 2015-01-21 22:48:18 --> 404 Page Not Found --> temp_upload
+ERROR - 2015-01-21 22:54:35 --> 404 Page Not Found --> temp_upload
+ERROR - 2015-01-21 23:18:36 --> Severity: Warning  --> include_once(application/libraries/excel_reader2.php) [<a href='function.include-once'>function.include-once</a>]: failed to open stream: No such file or directory E:\Server\xampp\htdocs\e-kuesioner\application\controllers\rujukan\alumni.php 148
+ERROR - 2015-01-21 23:18:36 --> Severity: Warning  --> include_once() [<a href='function.include'>function.include</a>]: Failed opening 'application/libraries/excel_reader2.php' for inclusion (include_path='.;E:\Server\xampp\php\PEAR') E:\Server\xampp\htdocs\e-kuesioner\application\controllers\rujukan\alumni.php 148
+ERROR - 2015-01-21 23:19:26 --> Severity: Notice  --> Undefined index: fileexcel E:\Server\xampp\htdocs\e-kuesioner\application\controllers\rujukan\alumni.php 150
+ERROR - 2015-01-21 23:19:26 --> Severity: Notice  --> Undefined offset: 0 E:\Server\xampp\htdocs\e-kuesioner\application\libraries\excel_reader2.php 390
+ERROR - 2015-01-21 23:23:46 --> Severity: Notice  --> Undefined index: fileexcel E:\Server\xampp\htdocs\e-kuesioner\application\controllers\rujukan\alumni.php 150
+ERROR - 2015-01-21 23:23:46 --> Severity: Notice  --> Undefined offset: 0 E:\Server\xampp\htdocs\e-kuesioner\application\libraries\excel_reader2.php 390
+ERROR - 2015-01-21 23:27:40 --> Severity: Notice  --> Undefined index: fileexcel E:\Server\xampp\htdocs\e-kuesioner\application\controllers\rujukan\alumni.php 150
+ERROR - 2015-01-21 23:27:40 --> Severity: Notice  --> Undefined offset: 0 E:\Server\xampp\htdocs\e-kuesioner\application\libraries\excel_reader2.php 390
